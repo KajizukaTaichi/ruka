cb0dfc8a7bf3a429bcf38dd1c9e3ed604f114a60
梶塚太智 <kajizukataichi@outlook.jp>
2025-05-05 01:31:21 +0900

    Revert "Update parse.rs"
    This reverts commit 13c5883774415f6afedb08434950e2ab1425c271.

--- forth/src/parse.rs
+++ forth/src/parse.rs
@@ -7,83 +7,67 @@ pub fn parse(tokens: Vec<Token>) -> Option<Vec<TopLevel>> {
         Body,
     }
     #[derive(Clone, Debug)]
-    enum CtrlState {
+    enum IfState {
         Condition,
         Then,
         Else,
-        While,
     }
 
     let mut word_state = WordState::Name;
-    let mut ctrl_state = CtrlState::Condition;
+    let mut if_state = IfState::Condition;
 
     let mut temp_name = None;
     let mut temp_body: Vec<Node> = vec![];
     let mut temp_then: Vec<Node> = vec![];
     let mut temp_else: Vec<Node> = vec![];
-    let mut temp_loop: Vec<Node> = vec![];
     let mut expr = Vec::new();
 
     for token in tokens {
-        match (token.clone(), word_state.clone(), ctrl_state.clone()) {
-            (Token::Word(name), WordState::Name, CtrlState::Condition) => {
+        match (token.clone(), word_state.clone(), if_state.clone()) {
+            (Token::Word(name), WordState::Name, IfState::Condition) => {
                 temp_name = Some(name.to_owned())
             }
-            (Token::DefineStart, WordState::Name, CtrlState::Condition) => {
+            (Token::DefineStart, WordState::Name, IfState::Condition) => {
                 word_state = WordState::Body;
             }
-            (Token::DefineEnd, WordState::Body, CtrlState::Condition) => {
+            (Token::DefineEnd, WordState::Body, IfState::Condition) => {
                 expr.push(TopLevel::Define(temp_name.clone()?, temp_body.clone()));
                 word_state = WordState::Name;
                 temp_name = None;
                 temp_body.clear()
             }
-            (Token::Number(n), WordState::Body, CtrlState::Condition) => {
+            (Token::Number(n), WordState::Body, IfState::Condition) => {
                 temp_body.push(Node::Value(n))
             }
-            (Token::Word(name), WordState::Body, CtrlState::Condition) => {
+            (Token::Word(name), WordState::Body, IfState::Condition) => {
                 temp_body.push(Node::Call(Word::parse(&name)?))
             }
-            (Token::IfThen, WordState::Body, CtrlState::Condition) => {
-                ctrl_state = CtrlState::Then;
+            (Token::IfThen, WordState::Body, IfState::Condition) => {
+                if_state = IfState::Then;
             }
-            (Token::WhileStart, WordState::Body, CtrlState::Condition) => {
-                ctrl_state = CtrlState::While;
-            }
-            (Token::IfElse, WordState::Body, CtrlState::Then) => {
-                ctrl_state = CtrlState::Else;
-            }
-            (Token::Number(n), WordState::Body, CtrlState::Then) => {
+            (Token::Number(n), WordState::Body, IfState::Then) => {
                 temp_then.push(Node::Value(n));
             }
-            (Token::Word(name), WordState::Body, CtrlState::Then) => {
+            (Token::Word(name), WordState::Body, IfState::Then) => {
                 temp_then.push(Node::Call(Word::parse(&name)?))
             }
-            (Token::Number(n), WordState::Body, CtrlState::Else) => {
+            (Token::IfElse, WordState::Body, IfState::Then) => {
+                if_state = IfState::Else;
+            }
+            (Token::Number(n), WordState::Body, IfState::Else) => {
                 temp_else.push(Node::Value(n));
             }
-            (Token::Word(name), WordState::Body, CtrlState::Else) => {
+            (Token::Word(name), WordState::Body, IfState::Else) => {
                 temp_else.push(Node::Call(Word::parse(&name)?))
-            }
-            (Token::Number(n), WordState::Body, CtrlState::While) => {
-                temp_loop.push(Node::Value(n));
-            }
-            (Token::Word(name), WordState::Body, CtrlState::While) => {
-                temp_loop.push(Node::Call(Word::parse(&name)?))
             }
             (Token::IfEnd, WordState::Body, _) => {
                 temp_body.push(Node::If(temp_then.clone(), temp_else.clone()));
-                ctrl_state = CtrlState::Condition;
+                if_state = IfState::Condition;
                 temp_then.clear();
                 temp_else.clear();
             }
-            (Token::WhileEnd, WordState::Body, _) => {
-                temp_body.push(Node::While(temp_loop.clone()));
-                ctrl_state = CtrlState::Condition;
-                temp_loop.clear();
-            }
             _ => {
-                dbg!((token.clone(), &word_state, &ctrl_state));
+                dbg!((token.clone(), &word_state, &if_state));
                 return None;
             }
         }
