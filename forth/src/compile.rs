94fac804421cd61d3d398dbde5677a16d73027ad
梶塚太智 <kajizukataichi@outlook.jp>
2025-05-04 22:11:47 +0900

    Update compile.rs

--- forth/src/compile.rs
+++ forth/src/compile.rs
@@ -26,7 +26,22 @@ impl Node {
                     "\tpop cr\n\tjmp cr, then_{label}\n\tjmp 1, else_{label}\nthen_{label}:\n{then}\tjmp 1, end_{label}\nelse_{label}:\n{else_}end_{label}:\n",
                 )
             }
-            Node::Call(word) => word.compile(ctx),
+            Node::Call(word) => word.compile(),
+        }
+    }
+}
+
+impl Word {
+    fn compile(&self) -> String {
+        match self {
+            Word::Add => format!("\tpop dr\n\tpop ar\n\tadd ar, dr\n\tpsh ar\n"),
+            Word::Mul => format!("\tpop dr\n\tpop ar\n\tmul ar, dr\n\tpsh ar\n"),
+            Word::Sub => format!("\tpop dr\n\tpop ar\n\tneg dr\n\tadd ar, dr\n\tpsh ar\n"),
+            Word::Div => format!("\tpop dr\n\tpop ar\n\tinv dr\n\tadd ar, dr\n\tpsh ar\n"),
+            Word::Equal => format!("\tpop dr\n\tpop ar\n\teql ar, dr\n\tpsh ar\n"),
+            Word::LessThan => format!("\tpop dr\n\tpop ar\n\tles ar, dr\n\tpsh ar\n"),
+            Word::GreaterThan => format!("\tpop ar\n\tpop dr\n\tles ar, dr\n\tpsh ar\n"),
+            Word::User(name) => format!("\tcal word_{name}\n"),
         }
     }
 }
