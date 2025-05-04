86cb6ca4723b812c702c83b9d62c3a9f19ab9a7c
梶塚太智 <kajizukataichi@outlook.jp>
2025-05-04 22:21:31 +0900

    Update main.rs

--- forth/src/main.rs
+++ forth/src/main.rs
@@ -5,18 +5,19 @@ mod parse;
 use compile::Context;
 use lexer::Token;
 use parse::parse;
+use ruka_vm::*;
 
 fn main() {
-    println!("Hello, world!");
-    println!(
-        "{}",
-        compile!(
-            parse(lexer::tokenize(
-                "メイン　とは　1　2 足 こと"
-            ))
-            .unwrap() => &mut Context { label_index: 0 }
-        )
-    );
+    let code = "メイン　とは　1　2 足 こと";
+
+    let ast = parse(lexer::tokenize(code)).unwrap();
+    let output = compile!(ast => &mut Context { label_index: 0 });
+    let asm_code = format!("\tcal word_メイン\n\thlt\n{output}");
+    println!("{asm_code}");
+
+    let mut vm = RukaVM::new(asm(&asm_code).unwrap());
+    vm.run();
+    vm.dump();
 }
 
 type Expr = Vec<Node>;
