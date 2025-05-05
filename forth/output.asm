f3a788520693ba27eb5bb913b8364070782222db
梶塚太智 <kajizukataichi@outlook.jp>
2025-05-05 21:05:47 +0900

    Create output.asm

---
+++ forth/output.asm
@@ -0,0 +1,48 @@
+	cal word_main
+	hlt
+word_dup:
+	psh 2
+	pop dr
+	pop ar
+	mul ar, dr
+	psh ar
+	ret
+word_half:
+	psh 2
+	pop dr
+	pop ar
+	inv dr
+	mul ar, dr
+	psh ar
+	ret
+word_main:
+	psh 5
+	cal word_dup
+	cal word_dup
+	cal word_half
+	psh 10
+	pop dr
+	pop ar
+	eql ar, dr
+	psh ar
+	pop cr
+	jmp cr, then_0
+	jmp 1, else_0
+then_0:
+	psh 1
+	psh 2
+	jmp 1, end_0
+else_0:
+	psh 3
+	psh 4
+end_0:
+	pop dr
+	pop ar
+	add ar, dr
+	psh ar
+	cal word_half
+	psh 8
+	pop ba
+	pop ar
+	sta ba, ar
+	ret
