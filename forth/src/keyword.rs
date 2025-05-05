09eb5462456f56d5480df284f908f8aa58150d69
梶塚太智 <kajizukataichi@outlook.jp>
2025-05-05 18:05:02 +0900

    Update keyword.rs

--- forth/src/keyword.rs
+++ forth/src/keyword.rs
@@ -1,14 +1,38 @@
-pub const ADD: &str = "足";
-pub const SUB: &str = "引";
-pub const MUL: &str = "掛";
-pub const DIV: &str = "割";
-pub const EQUAL: &str = "等";
-pub const LESS_THAN: &str = "小";
-pub const GREATER_THAN: &str = "大";
-pub const LOAD: &str = "読";
-pub const STORE: &str = "書";
-pub const DEFINE_START: &str = "とは";
-pub const DEFINE_END: &str = "こと。";
-pub const IF_THEN: &str = "ならば";
-pub const IF_ELSE: &str = "でなければ";
-pub const IF_END: &str = "つぎに";
+#[derive(Clone, Debug)]
+pub struct Keyword {
+    pub add: String,
+    pub sub: String,
+    pub mul: String,
+    pub div: String,
+    pub equal: String,
+    pub less_than: String,
+    pub greater_than: String,
+    pub load: String,
+    pub store: String,
+    pub define_start: String,
+    pub define_end: String,
+    pub if_then: String,
+    pub if_else: String,
+    pub if_end: String,
+}
+
+impl Keyword {
+    pub fn japanese() -> Self {
+        Keyword {
+            add: "足".to_string(),
+            sub: "引".to_string(),
+            mul: "掛".to_string(),
+            div: "割".to_string(),
+            equal: "等".to_string(),
+            less_than: "小".to_string(),
+            greater_than: "大".to_string(),
+            load: "読".to_string(),
+            store: "書".to_string(),
+            define_start: "とは".to_string(),
+            define_end: "こと。".to_string(),
+            if_then: "ならば".to_string(),
+            if_else: "でなければ".to_string(),
+            if_end: "つぎに".to_string(),
+        }
+    }
+}
