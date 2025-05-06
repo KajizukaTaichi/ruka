3403d21989f9d0e80f70a750de2b5a0b93f000ed
梶塚太智 <kajizukataichi@outlook.jp>
2025-05-06 22:13:18 +0900

    Create main.rs

---
+++ lisp/src/main.rs
@@ -0,0 +1,40 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+enum Expr {
+    List(Vec<Expr>),
+    Symbol(String),
+    Value(f64),
+}
+
+impl Expr {
+    fn compile(&self) -> Option<String> {
+        Some(match self {
+            Expr::Value(literal) => format!("\tmov ar, {literal}\n"),
+            Expr::List(list) => match list.first()? {
+                Expr::Symbol(symbol) => {
+                    macro_rules! multi_args {
+                        ($name: expr => $list:expr) => {{
+                            let mut result = String::new();
+                            result.push_str("\tmov ar, 0\n");
+                            for expr in $list.iter().skip(1) {
+                                result.push_str("\tpsh ar\n");
+                                result.push_str(&expr.compile()?);
+                                result.push_str(&format!("\tpop dr\n\t{} ar, dr\n", $name));
+                            }
+                            result
+                        }};
+                    }
+                    match symbol.as_str() {
+                        "+" => multi_args!("add" => list),
+                        "*" => multi_args!("mul" => list),
+                        _ => return None,
+                    }
+                }
+                _ => return None,
+            },
+            _ => return None,
+        })
+    }
+}
