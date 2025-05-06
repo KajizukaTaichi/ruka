b32c4ae2a3bc557a88e28edcf22ce78de91be949
梶塚太智 <kajizukataichi@outlook.jp>
2025-05-06 10:46:18 +0900

    Update output.asm

--- forth/output.asm
+++ forth/output.asm
@@ -1,100 +1,50 @@
-	cal word_始まり
+	cal word_главное
 	hlt
-word_倍:
+word_двойной:
 	psh 2
-	cal word_を
 	pop dr
 	pop ar
 	mul ar, dr
 	psh ar
-	cal word_ける
 	ret
-word_半分:
+word_половина:
 	psh 2
-	cal word_で
 	pop dr
 	pop ar
 	inv dr
 	mul ar, dr
 	psh ar
-	cal word_る
 	ret
-word_一時記憶:
-	psh 8
-	cal word_番地の
-	ret
-word_覚:
-	cal word_一時記憶
-	cal word_領域に
-	pop ba
-	pop ar
-	sta ba, ar
-	cal word_き込む
-	ret
-word_思出:
-	cal word_一時記憶
-	cal word_領域に
-	pop ba
-	lda ar, ba
-	psh ar
-	cal word_み込む
-	ret
-word_始まり:
+word_главное:
 	psh 5
-	cal word_の
-	cal word_倍
-	cal word_の
-	cal word_倍
-	cal word_の
-	cal word_半分
-	cal word_が
+	cal word_двойной
+	cal word_двойной
+	cal word_половина
 	psh 10
-	cal word_と
 	pop dr
 	pop ar
 	eql ar, dr
 	psh ar
-	cal word_しい
 	pop cr
 	jmp cr, then_0
 	jmp 1, else_0
 then_0:
 	psh 1
-	cal word_と
+	cal word_и
 	psh 2
-	cal word_を
 	jmp 1, end_0
 else_0:
 	psh 3
-	cal word_と
+	cal word_и
 	psh 4
-	cal word_を
 end_0:
 	pop dr
 	pop ar
 	add ar, dr
 	psh ar
-	cal word_しあわせて
-	cal word_半分
-	cal word_にして
-	cal word_覚
-	cal word_え、
-	psh 10
-	cal word_と
-	psh 10
-	cal word_を
-	pop dr
+	cal word_половина
+	psh 8
+	pop ba
 	pop ar
-	mul ar, dr
-	psh ar
-	cal word_けて
-	cal word_倍
-	cal word_したものに
-	cal word_思出
-	cal word_して
-	pop dr
-	pop ar
-	mul ar, dr
-	psh ar
-	cal word_ける
+	sta ba, ar
 	ret
