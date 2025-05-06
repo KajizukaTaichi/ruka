445472583828597f87a578a90b4bc7f896fe0571
梶塚太智 <kajizukataichi@outlook.jp>
2025-05-06 23:01:06 +0900

    Update main.rs

--- lisp/src/main.rs
+++ lisp/src/main.rs
@@ -31,6 +31,8 @@ enum Expr {
 }
 
 impl Expr {
+    fn parse(source: &str) -> Option<Expr> {}
+
     fn compile(&self) -> Option<String> {
         Some(match self {
             Expr::Value(literal) => format!("\tmov ar, {literal}\n"),
@@ -59,4 +61,65 @@ impl Expr {
             _ => return None,
         })
     }
+}
+
+fn tokenize(input: &str, delimiter: &[char]) -> Option<Vec<String>> {
+    let mut tokens: Vec<String> = Vec::new();
+    let mut current_token = String::new();
+    let mut in_parentheses: usize = 0;
+    let mut in_quote = false;
+    let mut is_escape = false;
+
+    for c in input.chars() {
+        if is_escape {
+            current_token.push(match c {
+                'n' => '\n',
+                't' => '\t',
+                'r' => '\r',
+                _ => c,
+            });
+            is_escape = false;
+        } else {
+            match c {
+                '(' | '{' | '[' if !in_quote => {
+                    current_token.push(c);
+                    in_parentheses += 1;
+                }
+                ')' | '}' | ']' if !in_quote => {
+                    current_token.push(c);
+                    in_parentheses.checked_sub(1).map(|x| in_parentheses = x);
+                }
+                '"' | '\'' | '`' => {
+                    in_quote = !in_quote;
+                    current_token.push(c);
+                }
+                '\\' if in_quote => {
+                    current_token.push(c);
+                    is_escape = true;
+                }
+                other => {
+                    if delimiter.contains(&other) && !in_quote {
+                        if in_parentheses != 0 {
+                            current_token.push(c);
+                        } else if !current_token.is_empty() {
+                            tokens.push(current_token.clone());
+                            current_token.clear();
+                        }
+                    } else {
+                        current_token.push(c);
+                    }
+                }
+            }
+        }
+    }
+
+    // Syntax error check
+    if is_escape || in_quote || in_parentheses != 0 {
+        return None;
+    }
+    if !current_token.is_empty() {
+        tokens.push(current_token.clone());
+        current_token.clear();
+    }
+    Some(tokens)
 }
