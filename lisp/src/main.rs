cb72d05a46b111095832d45f59944196f6403551
梶塚太智 <kajizukataichi@outlook.jp>
2025-05-06 23:14:59 +0900

    Update main.rs

--- lisp/src/main.rs
+++ lisp/src/main.rs
@@ -23,26 +23,6 @@ enum Expr {
 }
 
 impl Expr {
-    fn parse(source: &str) -> Option<Expr> {
-        let source = source.trim();
-        if let Ok(n) = source.parse::<f64>() {
-            Some(Expr::Value(n))
-        } else if let Some(source) = source
-            .strip_prefix("(")
-            .map(|x| x.strip_suffix(")"))
-            .flatten()
-        {
-            Some(Expr::List(
-                tokenize(source)?
-                    .iter()
-                    .map(|x| Expr::parse(x))
-                    .collect::<Option<Vec<Expr>>>()?,
-            ))
-        } else {
-            Some(Expr::Symbol(source.to_string()))
-        }
-    }
-
     fn compile(&self) -> Option<String> {
         Some(match self {
             Expr::Value(literal) => format!("\tmov ar, {literal}\n"),
@@ -70,6 +50,26 @@ impl Expr {
             },
             _ => return None,
         })
+    }
+
+    fn parse(source: &str) -> Option<Expr> {
+        let source = source.trim();
+        if let Ok(n) = source.parse::<f64>() {
+            Some(Expr::Value(n))
+        } else if let Some(source) = source
+            .strip_prefix("(")
+            .map(|x| x.strip_suffix(")"))
+            .flatten()
+        {
+            Some(Expr::List(
+                tokenize(source)?
+                    .iter()
+                    .map(|x| Expr::parse(x))
+                    .collect::<Option<Vec<Expr>>>()?,
+            ))
+        } else {
+            Some(Expr::Symbol(source.to_string()))
+        }
     }
 }
 
