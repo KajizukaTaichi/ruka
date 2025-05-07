105058c385dbde832ecfd4441c7ba25fd42f470a
梶塚太智 <kajizukataichi@outlook.jp>
2025-05-07 18:15:53 +0900

    Update main.rs

--- lisp/src/main.rs
+++ lisp/src/main.rs
@@ -8,23 +8,16 @@ use ruka_vm::{BasedMode, RukaVM, asm};
 
 fn main() {
     println!("Hello, world!");
-    run("(* 2 (- 10 5) (+ 1 2 3))").map(|x| println!(" = {x}"));
+    run("(var x 10) (+ x 2)").map(|x| println!(" = {x}"));
 }
 
 fn run(source: &str) -> Option<f64> {
     let env = &mut IndexMap::new();
     let code = tokenize(source)?
         .iter()
-        .map(|code| Expr::parse(&code).map(|ast| ast.compile(env)))
-        .flatten()
+        .map(|code| Expr::parse(&code).and_then(|ast| ast.compile(env)))
         .collect::<Option<Vec<_>>>()?;
-    let code = format!(
-        "{}\thlt\n{}",
-        code.last()?,
-        code.get(..code.len() - 1)
-            .map(|x| x.join("\n\n"))
-            .unwrap_or(String::new())
-    );
+    let code = format!("{}\thlt\n", code.concat());
 
     println!("{code}");
 
