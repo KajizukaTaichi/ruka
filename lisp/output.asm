595847243e64e815567476c0aa4e1ec01828610b
梶塚太智 <kajizukataichi@outlook.jp>
2025-05-07 22:58:46 +0900

    Update output.asm

--- lisp/output.asm
+++ lisp/output.asm
@@ -10,6 +10,21 @@ function_inc:
 	add ar, dr
 	ret
 end_inc:
+	jmp 1, end_sub
+function_sub:
+	pop ar
+	sta 1, ar	; b
+	pop ar
+	sta 2, ar	; a
+	lda ar, 2	; a
+	psh ar
+	lda ar, 1	; b
+	mov dr, ar
+	pop ar
+	neg dr
+	add ar, dr
+	ret
+end_sub:
 	mov ar, 2
 	psh ar
 	mov ar, 3
@@ -18,4 +33,8 @@ end_inc:
 	mul ar, dr
 	psh ar
 	cal function_inc
+	psh ar
+	mov ar, 6
+	psh ar
+	cal function_sub
 	hlt
