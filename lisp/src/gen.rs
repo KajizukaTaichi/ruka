9d3323b901c475afaf18e9a24914588fb050b686
梶塚太智 <kajizukataichi@outlook.jp>
2025-05-07 22:35:54 +0900

    Update gen.rs

--- lisp/src/gen.rs
+++ lisp/src/gen.rs
@@ -6,7 +6,7 @@ impl Expr {
     pub fn compile(&self, env: &mut Env) -> Option<String> {
         Some(match self {
             Expr::Value(literal) => format!("\tmov ar, {literal}\n"),
-            Expr::Symbol(name) => format!("\tlda ar, {}\r; {name}\n", env.get(name)?),
+            Expr::Symbol(name) => format!("\tlda ar, {}\t; {name}\n", env.get(name)?),
             Expr::List(list) => match list.first()? {
                 Expr::Symbol(symbol) => {
                     macro_rules! multi_args {
