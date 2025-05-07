870b4c1fb077b5fd363b563576e278088e5604be
梶塚太智 <kajizukataichi@outlook.jp>
2025-05-07 18:18:02 +0900

    Update output.asm

--- lisp/output.asm
+++ lisp/output.asm
@@ -2,7 +2,25 @@
 	sta 0, ar
 	lda ar, 0
 	psh ar
+	mov ar, 7
+	mov dr, ar
+	pop ar
+	neg dr
+	add ar, dr
+	sta 1, ar
+	lda ar, 0
+	psh ar
 	mov ar, 2
+	psh ar
+	lda ar, 1
+	mov dr, ar
+	pop ar
+	mul ar, dr
+	mov dr, ar
+	pop ar
+	add ar, dr
+	psh ar
+	mov ar, 8
 	mov dr, ar
 	pop ar
 	add ar, dr
