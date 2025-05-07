948c48636daaa0ae034c3bfb5724151a0c0bc83e
梶塚太智 <kajizukataichi@outlook.jp>
2025-05-07 12:55:12 +0900

    Update gen.rs

--- lisp/src/gen.rs
+++ lisp/src/gen.rs
@@ -7,9 +7,9 @@ impl Expr {
             Expr::List(list) => match list.first()? {
                 Expr::Symbol(symbol) => {
                     macro_rules! multi_args {
-                        ($name: expr => $list: expr, $init: expr) => {{
+                        ($name: expr => $list: expr) => {{
                             let mut result = String::new();
-                            result.push_str(&format!("\tmov ar, {}\n", $init));
+                            result.push_str(&$list.get(1)?.compile()?);
                             for expr in $list.iter().skip(1) {
                                 result.push_str("\tpsh ar\n");
                                 result.push_str(&expr.compile()?);
@@ -19,8 +19,10 @@ impl Expr {
                         }};
                     }
                     match symbol.as_str() {
-                        "+" => multi_args!("add" => list, 0.0),
-                        "*" => multi_args!("mul" => list, 1.0),
+                        "+" => multi_args!("add" => list),
+                        "*" => multi_args!("mul" => list),
+                        "-" => multi_args!("neg, dr\nadd" => list),
+                        "/" => multi_args!("inv, dr\nmul" => list),
                         _ => return None,
                     }
                 }
