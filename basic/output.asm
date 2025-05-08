2ac565782e96fba8ed71d468e6e0ea24f05f5312
梶塚太智 <kajizukataichi@outlook.jp>
2025-05-08 11:00:02 +0900

    Update output.asm

--- basic/output.asm
+++ basic/output.asm
@@ -1,2 +1,27 @@
 	mov ar, 1
 	sta 0, ar	; a
+	mov ar, 5
+	psh ar
+	lda ar, 0	; a
+	mov dr, ar
+	pop ar
+	les ar, dr
+	mov cr, ar
+	jmp cr, if_then_0
+	jmp 1, if_else_0
+if_then_0:
+	jmp 1, if_end_0
+if_end_0:if_else_0:
+if_end_0:	mov ar, 5
+	psh ar
+	lda ar, 0	; a
+	mov dr, ar
+	pop ar
+	les ar, dr
+	mov cr, ar
+	jmp cr, if_then_1
+	jmp 1, if_else_1
+if_then_1:
+	jmp 1, if_end_1
+if_end_1:if_else_1:
+if_end_1:
